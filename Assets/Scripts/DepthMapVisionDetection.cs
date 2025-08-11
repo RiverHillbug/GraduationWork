b558d42ec716abe166c1dfb39bd7c48733ff0014
Seher "River" Sevik <sehersevik00@gmail.com>
2025-08-11 13:35:47 +0200

    Fixed depth map detection optimization technique

--- Assets/Scripts/DepthMapVisionDetection.cs
+++ Assets/Scripts/DepthMapVisionDetection.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.IO;
 using UnityEngine;
-using UnityEngine.Pool;
 
 public class DepthMapVisionDetection : MonoBehaviour
 {
@@ -26,16 +25,25 @@ public class DepthMapVisionDetection : MonoBehaviour
 	[SerializeField]
 	private LayerMask m_TargetsLayer = 0;
 
+	[SerializeField]
+	private bool m_UseShapeDetectionOptimization = true;
+
+	[SerializeField]
+	private Texture2D m_TargetsDepthMap = null;
+	private bool m_ProvidedTestTexture = false;
+
 	private RenderTexture m_TargetsDepthTexture = null;
 	private RenderTexture m_EnvironmentDepthTexture = null;
 	private Texture2D m_EnvironmentDepthMap = null;
-	private Texture2D m_TargetsDepthMap = null;
 
 	private readonly List<VisionAgent> m_RegisteredAgents = new();
 	private int m_CurrentAgentIndex = 0;
 
 	private readonly List<VisionTarget> m_VisionTargetsInRange = new();
 	private readonly HashSet<VisionTarget> m_CurrentlyDetectedTargets = new();
+	private readonly HashSet<Vector2Int> m_AlreadyDetectedPixels = new();
+	private readonly HashSet<Vector2Int> m_AlreadyCheckedPixels = new();
+	private readonly Queue<Vector2Int> m_NextVerticalChecks = new();
 
 	private void Awake()
 	{
@@ -46,11 +54,33 @@ public class DepthMapVisionDetection : MonoBehaviour
 		int textureWidth = Mathf.RoundToInt(m_Camera.pixelWidth * m_TexturesSizeMultiplier);
 		int textureHeight = Mathf.RoundToInt(m_Camera.pixelHeight * m_TexturesSizeMultiplier);
 
-		m_TargetsDepthTexture = new RenderTexture(textureWidth, textureHeight, 24);
-		m_EnvironmentDepthTexture = new RenderTexture(textureWidth, textureHeight, 24);
+		if (m_TargetsDepthMap != null)
+		{
+			textureWidth = m_TargetsDepthMap.width;
+			textureHeight = m_TargetsDepthMap.height;
+			m_ProvidedTestTexture = true;
+		}
 
-		m_TargetsDepthMap = new(textureWidth, textureHeight, TextureFormat.RFloat, false);
+		if (!m_ProvidedTestTexture)
+		{
+			m_TargetsDepthTexture = new RenderTexture(textureWidth, textureHeight, 24);
+			m_EnvironmentDepthTexture = new RenderTexture(textureWidth, textureHeight, 24);
+		}
+
+		if (!m_ProvidedTestTexture)
+			m_TargetsDepthMap = new(textureWidth, textureHeight, TextureFormat.RFloat, false);
 		m_EnvironmentDepthMap = new(textureWidth, textureHeight, TextureFormat.RFloat, false);
+
+		if (m_ProvidedTestTexture)
+		{
+			for (int y = 0; y < textureHeight; ++y)
+			{
+				for (int x = 0; x < textureWidth; ++x)
+				{
+					m_EnvironmentDepthMap.SetPixel(x, y, Color.white);
+				}
+			}
+		}
 	}
 
 	private void Update()
@@ -88,8 +118,12 @@ public class DepthMapVisionDetection : MonoBehaviour
 		m_Camera.enabled = true;
 
 		DetectVisionTargetsInRange();
-		RenderEnvironment();
-		RenderTargets();
+
+		if (!m_ProvidedTestTexture)
+		{
+			RenderEnvironment();
+			RenderTargets();
+		}
 
 		m_Camera.enabled = false;
 
@@ -146,21 +180,33 @@ public class DepthMapVisionDetection : MonoBehaviour
 	private bool CompareTextures()
 	{
 		m_CurrentlyDetectedTargets.Clear();
+		m_AlreadyDetectedPixels.Clear();
+		m_AlreadyCheckedPixels.Clear();
+
+		int raycastsCount = 0;
 
 		for (int y = 0; y < m_EnvironmentDepthMap.height; ++y)
 		{
 			for (int x = 0; x < m_EnvironmentDepthMap.width; ++x)
 			{
+				if (m_UseShapeDetectionOptimization && m_AlreadyDetectedPixels.Contains(new(x, y)))
+					continue;
+
 				float envDepth = m_EnvironmentDepthMap.GetPixel(x, y).r;
 				float targetDepth = m_TargetsDepthMap.GetPixel(x, y).r;
 
 				if (targetDepth < envDepth)
 				{
+					++raycastsCount;
 					RaycastOnTarget((float)x / m_TargetsDepthMap.width, (float)y / m_TargetsDepthMap.height, targetDepth * (m_Camera.farClipPlane - m_Camera.nearClipPlane));
-					//EraseTargetShape(x, y); // Needs some fixing (make it not recursive, mostly)
+
+					if (m_UseShapeDetectionOptimization)
+						DetectShapePixels(x, y);
 				}
 			}
 		}
+
+		Debug.Log($"Raycaughst {raycastsCount} times");
 
 		return m_CurrentlyDetectedTargets.Count > 0;
 	}
@@ -190,77 +236,215 @@ public class DepthMapVisionDetection : MonoBehaviour
 		}
 	}
 
-	private void EraseTargetShape(int x, int y)
+	private void DetectShapePixels(int x, int y)
 	{
-		// Closed list are the pixels that have or are being treated already.
-		// It's a hashset because we just need to know that they are in there, we don't iterate over them from it.
-		HashSet<Vector2Int> closedList = HashSetPool<Vector2Int>.Get();
+		//float startingDepth = m_TargetsDepthMap.GetPixel(x, y).r;
+		Vector2Int startingPixel = new(x, y);
 
-		float startingDepth = m_TargetsDepthMap.GetPixel(x, y).r;
-		EraseSurroundingSimilarPixels(new(x, y), startingDepth, ref closedList);
-		m_TargetsDepthMap.SetPixel(x, y, Color.white);
+		//m_AlreadyDetectedPixels.Add(startingPixel);
+		//DetectSurroundingSimilarPixels(startingPixel, startingDepth);
+		DetectSurroundingSimilarPixelsByMarching(startingPixel);
 
-		closedList.Clear();
-		HashSetPool<Vector2Int>.Release(closedList);
+		/*int textureWidth = m_TargetsDepthMap.width;
+		int textureHeight = m_TargetsDepthMap.height;
+		Texture2D outputTest = new(textureWidth, textureHeight, TextureFormat.RFloat, false);
 
-		/*byte[] bytesAfter = m_TargetsDepthMap.EncodeToPNG();
-		File.WriteAllBytes(Application.dataPath + "/After.png", bytesAfter);*/
+		for (y = 0; y < textureHeight; ++y)
+		{
+			for (x = 0; x < textureWidth; ++x)
+			{
+				outputTest.SetPixel(x, y, m_AlreadyDetectedPixels.Contains(new(x, y)) ? Color.black : Color.white);
+			}
+		}
+
+		byte[] bytes = outputTest.EncodeToPNG();
+		File.WriteAllBytes(Application.dataPath + "/Detected.png", bytes);*/
 	}
 
-	private void EraseSurroundingSimilarPixels(Vector2Int startingPixel,float startingDepth, ref HashSet<Vector2Int> closedList)
+	private void DetectSurroundingSimilarPixels(Vector2Int startingPixel,float startingDepth)
 	{
 		const float depthDifferenceToConsiderDifferentEntity = 0.01f;
 
 		Vector2Int right = new(startingPixel.x + 1, startingPixel.y);
-		if (!closedList.Contains(right) && startingPixel.x + 1 < m_TargetsDepthMap.width)
+		if (!m_AlreadyDetectedPixels.Contains(right) && !m_AlreadyCheckedPixels.Contains(right) && right.x < m_TargetsDepthMap.width)
 		{
-			closedList.Add(right);
+			m_AlreadyCheckedPixels.Add(right);
 
 			float depth = m_TargetsDepthMap.GetPixel(right.x, right.y).r;
 			if (Mathf.Abs(depth - startingDepth) <= depthDifferenceToConsiderDifferentEntity)
 			{
-				m_TargetsDepthMap.SetPixel(right.x, right.y, Color.white);
-				EraseSurroundingSimilarPixels(right, depth, ref closedList);
+				m_AlreadyDetectedPixels.Add(right);
+				DetectSurroundingSimilarPixels(right, depth);
 			}
 		}
 
 		Vector2Int up = new(startingPixel.x, startingPixel.y + 1);
-		if (!closedList.Contains(up) && startingPixel.y + 1 < m_TargetsDepthMap.height)
+		if (!m_AlreadyDetectedPixels.Contains(up) && !m_AlreadyCheckedPixels.Contains(up) && up.y < m_TargetsDepthMap.height)
 		{
-			closedList.Add(up);
+			m_AlreadyCheckedPixels.Add(up);
 
 			float depth = m_TargetsDepthMap.GetPixel(up.x, up.y).r;
 			if (Mathf.Abs(depth - startingDepth) <= depthDifferenceToConsiderDifferentEntity)
 			{
-				m_TargetsDepthMap.SetPixel(up.x, up.y, Color.white);
-				EraseSurroundingSimilarPixels(up, depth, ref closedList);
+				m_AlreadyDetectedPixels.Add(up);
+				DetectSurroundingSimilarPixels(up, depth);
 			}
 		}
 
 		Vector2Int left = new(startingPixel.x - 1, startingPixel.y);
-		if (!closedList.Contains(left) && startingPixel.x - 1 >= 0)
+		if (!m_AlreadyDetectedPixels.Contains(left) && !m_AlreadyCheckedPixels.Contains(left) && left.x >= 0)
 		{
-			closedList.Add(left);
+			m_AlreadyCheckedPixels.Add(left);
 
 			float depth = m_TargetsDepthMap.GetPixel(left.x, left.y).r;
 			if (Mathf.Abs(depth - startingDepth) <= depthDifferenceToConsiderDifferentEntity)
 			{
-				m_TargetsDepthMap.SetPixel(left.x, left.y, Color.white);
-				EraseSurroundingSimilarPixels(left, depth, ref closedList);
+				m_AlreadyDetectedPixels.Add(left);
+				DetectSurroundingSimilarPixels(left, depth);
 			}
 		}
 
 		Vector2Int down = new(startingPixel.x, startingPixel.y - 1);
-		if (!closedList.Contains(down) && startingPixel.y - 1 >= 0)
+		if (!m_AlreadyDetectedPixels.Contains(down) && !m_AlreadyCheckedPixels.Contains(down) && down.y >= 0)
 		{
-			closedList.Add(down);
+			m_AlreadyCheckedPixels.Add(down);
 
 			float depth = m_TargetsDepthMap.GetPixel(down.x, down.y).r;
 			if (Mathf.Abs(depth - startingDepth) <= depthDifferenceToConsiderDifferentEntity)
 			{
-				m_TargetsDepthMap.SetPixel(down.x, down.y, Color.white);
-				EraseSurroundingSimilarPixels(down, depth, ref closedList);
+				m_AlreadyDetectedPixels.Add(down);
+				DetectSurroundingSimilarPixels(down, depth);
 			}
 		}
+	}
+
+	private void DetectSurroundingSimilarPixelsByMarching(Vector2Int startingPixel)
+	{
+		bool isDone = false;
+		Vector2Int nextCheckLeft = -Vector2Int.one;
+		bool isMovingLeft = false;
+
+		bool isContinuousUp = false;
+		bool isContinuousDown = false;
+
+		Vector2Int currentPosition = startingPixel;
+		m_AlreadyDetectedPixels.Add(currentPosition);
+
+		do
+		{
+			float currentDepth = m_TargetsDepthMap.GetPixel(currentPosition.x, currentPosition.y).r;
+
+			Vector2Int up = new(currentPosition.x, currentPosition.y + 1);
+			if (!m_AlreadyCheckedPixels.Contains(up) && !m_AlreadyDetectedPixels.Contains(up) && up.y < m_TargetsDepthMap.height)
+			{
+				m_AlreadyCheckedPixels.Add(up);
+
+				if (IsPixelSimilarDepth(up, currentDepth))
+				{
+					m_AlreadyDetectedPixels.Add(up);
+
+					if (!isContinuousUp)
+						m_NextVerticalChecks.Enqueue(up);
+
+					isContinuousUp = true;
+				}
+				else
+				{
+					isContinuousUp = false;
+				}
+			}
+			else
+			{
+				isContinuousUp = m_AlreadyDetectedPixels.Contains(up);
+			}
+
+			Vector2Int down = new(currentPosition.x, currentPosition.y - 1);
+			if (!m_AlreadyCheckedPixels.Contains(down) && !m_AlreadyDetectedPixels.Contains(down) && down.y >= 0)
+			{
+				m_AlreadyCheckedPixels.Add(down);
+
+				if (IsPixelSimilarDepth(down, currentDepth))
+				{
+					m_AlreadyDetectedPixels.Add(down);
+
+					if (!isContinuousDown)
+						m_NextVerticalChecks.Enqueue(up);
+
+					isContinuousDown = true;
+				}
+				else
+				{
+					isContinuousDown = false;
+				}
+			}
+			else
+			{
+				isContinuousDown = m_AlreadyDetectedPixels.Contains(down);
+			}
+
+			currentPosition.x += (isMovingLeft ? -1 : 1);
+
+			bool cantMoveFurther = true;
+			if (!m_AlreadyCheckedPixels.Contains(currentPosition) && !m_AlreadyDetectedPixels.Contains(currentPosition) && currentPosition.x >= 0 && currentPosition.x < m_TargetsDepthMap.width)
+			{
+				m_AlreadyCheckedPixels.Add(currentPosition);
+
+				if (IsPixelSimilarDepth(currentPosition, currentDepth))
+				{
+					m_AlreadyDetectedPixels.Add(currentPosition);
+					cantMoveFurther = false;
+				}
+			}
+
+			if (cantMoveFurther)
+			{
+				if (nextCheckLeft != -Vector2Int.one)
+				{
+					currentPosition = nextCheckLeft;
+					nextCheckLeft = -Vector2Int.one;
+					isMovingLeft = true;
+
+					isContinuousUp = m_AlreadyDetectedPixels.Contains(currentPosition + new Vector2Int(1, 1));
+					isContinuousDown = m_AlreadyDetectedPixels.Contains(currentPosition + new Vector2Int(1, -1));
+				}
+				else
+				{
+					isMovingLeft = false;
+
+					if (m_NextVerticalChecks.Count > 0)
+					{
+						currentPosition = m_NextVerticalChecks.Dequeue();
+						currentDepth = m_TargetsDepthMap.GetPixel(currentPosition.x, currentPosition.y).r;
+
+						isContinuousUp = m_AlreadyDetectedPixels.Contains(currentPosition + Vector2Int.up);
+						isContinuousDown = m_AlreadyDetectedPixels.Contains(currentPosition + Vector2Int.down);
+
+						Vector2Int left = new(currentPosition.x - 1, currentPosition.y);
+						if (left.x >= 0)
+						{
+							m_AlreadyCheckedPixels.Add(left);
+
+							if (IsPixelSimilarDepth(left, currentDepth))
+							{
+								m_AlreadyDetectedPixels.Add(left);
+								nextCheckLeft = left;
+							}
+						}
+					}
+					else
+					{
+						isDone = true;
+					}
+				}
+			}
+		} while (!isDone);
+	}
+
+	private bool IsPixelSimilarDepth(Vector2Int pixel, float depthToCheckAgainst)
+	{
+		const float depthDifferenceToConsiderDifferentEntity = 0.01f;
+
+		float depth = m_TargetsDepthMap.GetPixel(pixel.x, pixel.y).r;
+		return Mathf.Abs(depth - depthToCheckAgainst) <= depthDifferenceToConsiderDifferentEntity;
 	}
 }
